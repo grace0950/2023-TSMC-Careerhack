f76a1faaacd108a237f56637d6878934c9c82a30
YJack0000 <jackleagends@gmail.com>
2023-02-11 12:35:50 +0800

    aq timout 3000

--- request-queue/db/mysql.js
+++ request-queue/db/mysql.js
@@ -8,7 +8,7 @@ const pool = createPool({
   database: "tsmc_storage",
   connectionLimit: 10,
   connectTimeout: 1000,
-  acquireTimeout: 10000,
+  acquireTimeout: 3000,
 });
 
 const poolQuery = async (sql, values) => {
