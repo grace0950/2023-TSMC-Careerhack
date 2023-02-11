3cb9bf0fddb8f3619b8efb691128b39c4f70da1d
YJack0000 <jackleagends@gmail.com>
2023-02-11 10:46:09 +0800

    timout querytime in bd

--- business/utils/mysql.js
+++ business/utils/mysql.js
@@ -7,6 +7,8 @@ const pool = mysql.createPool({
   password: "cerana",
   database: "tsmc_storage",
   connectionLimit: 10,
+  connectTimeout: 100,
+  queryTimeout: 1000,
 });
 
 const poolQuery = async (sql, values) => {
